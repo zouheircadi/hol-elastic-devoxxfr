42f73e81ae83261e06fa4ebb0fafddcc2d35f37d
zouheircadi <zouheir.cadi@gmail.com>
2019-03-24 16:58:59 +0100

    mdf

---
+++ data/post-data-1-curl.sh
@@ -0,0 +1,171 @@
+
+curl -XDELETE "http://localhost:9200/hol_devoxxfr_pm1"
+
+curl -XPUT "http://localhost:9200/hol_devoxxfr_pm1" -H 'Content-Type: application/json' -d'
+{
+  "mappings": 
+  {
+    "_doc" :
+    {
+      "properties" : 
+      {
+        "code" : 
+        {
+          "type" : "keyword"
+        },
+        "country" :
+        {
+          "type" : "keyword"
+        }
+      }
+    }
+  }
+}'
+
+curl -XPOST "http://localhost:9200/hol_devoxxfr_pm1/_doc/_bulk" -H 'Content-Type: application/json' -d'
+{ "index": {  }}
+{"code":"AFG","country":"Afghanistan"}
+{ "index": {  }}
+{"code":"ALB","country":"Albania"}
+{ "index": {  }}
+{"code":"DZA","country":"Algeria"}
+{ "index": {  }}
+{"code":"ASM","country":"American Samoa"}
+{ "index": {  }}
+{"code":"AND","country":"Andorra"}
+{ "index": {  }}
+{"code":"AGO","country":"Angola"}
+{ "index": {  }}
+{"code":"AIA","country":"Anguilla"}
+{ "index": {  }}
+{"code":"ATG","country":"Antigua & Barbuda"}
+{ "index": {  }}
+{"code":"ARG","country":"Argentina"}
+{ "index": {  }}
+{"code":"ARM","country":"Armenia"}
+{ "index": {  }}
+{"code":"ABW","country":"Aruba"}
+{ "index": {  }}
+{"code":"AUS","country":"Australia"}
+{ "index": {  }}
+{"code":"AUT","country":"Austria"}
+{ "index": {  }}
+{"code":"AZE","country":"Azerbaijan"}
+{ "index": {  }}
+{"code":"BHS","country":"Bahamas The"}
+{ "index": {  }}
+{"code":"BHR","country":"Bahrain"}
+{ "index": {  }}
+{"code":"BGD","country":"Bangladesh"}
+{ "index": {  }}
+{"code":"BRB","country":"Barbados"}
+{ "index": {  }}
+{"code":"BLR","country":"Belarus"}
+{ "index": {  }}
+{"code":"BEL","country":"Belgium"}
+{ "index": {  }}
+{"code":"BLZ","country":"Belize"}
+{ "index": {  }}
+{"code":"BEN","country":"Benin"}
+{ "index": {  }}
+{"code":"BMU","country":"Bermuda"}
+{ "index": {  }}
+{"code":"BTN","country":"Bhutan"}
+{ "index": {  }}
+{"code":"BOL","country":"Bolivia"}
+{ "index": {  }}
+{"code":"BIH","country":"Bosnia & Herzegovina"}
+{ "index": {  }}
+{"code":"BWA","country":"Botswana"}
+{ "index": {  }}
+{"code":"BRA","country":"Brazil"}
+{ "index": {  }}
+{"code":"IOT","country":"British Virgin Island"}
+{ "index": {  }}
+{"code":"BRN","country":"Brunei"}
+{ "index": {  }}
+{"code":"BGR","country":"Bulgaria"}
+{ "index": {  }}
+{"code":"BFA","country":"BurkinaFaso"}
+{ "index": {  }}
+{"code":"MMR","country":"Burma"}
+{ "index": {  }}
+{"code":"BDI","country":"Burundi"}
+{ "index": {  }}
+{"code":"KHM","country":"Cambodia"}
+{ "index": {  }}
+{"code":"CMR","country":"Cameroon"}
+{ "index": {  }}
+{"code":"CAN","country":"Canada"}
+{ "index": {  }}
+{"code":"CPV","country":"CapeVerde"}
+{ "index": {  }}
+{"code":"CYM","country":"CaymanIslands"}
+{ "index": {  }}
+{"code":"CAF","country":"Central African Republic"}
+{ "index": {  }}
+{"code":"TCD","country":"Chad"}
+{ "index": {  }}
+{"code":"CHL","country":"Chile"}
+{ "index": {  }}
+{"code":"CHN","country":"China"}
+{ "index": {  }}
+{"code":"COL","country":"Colombia"}
+{ "index": {  }}
+{"code":"COM","country":"Comoros"}
+{ "index": {  }}
+{"code":"COG","country":"Congo Democratic Republic"}
+{ "index": {  }}
+{"code":"COD","country":"Congo Republic of the"}
+{ "index": {  }}
+{"code":"COK","country":"Cook Islands"}
+{ "index": {  }}
+{"code":"CRI","country":"CostaRica"}
+{ "index": {  }}
+{"code":"CIV","country":"Cote d\"Ivoire"}
+{ "index": {  }}
+{"code":"HRV","country":"Croatia"}
+{ "index": {  }}
+{"code":"CUB","country":"Cuba"}
+{ "index": {  }}
+{"code":"CYP","country":"Cyprus"}
+{ "index": {  }}
+{"code":"CZE","country":"Czech Republic"}
+{ "index": {  }}
+{"code":"DNK","country":"Denmark"}
+{ "index": {  }}
+{"code":"DJI","country":"Djibouti"}
+{ "index": {  }}
+{"code":"DMA","country":"Dominica"}
+{ "index": {  }}
+{"code":"DOM","country":"Dominican Republic"}
+{ "index": {  }}
+{"code":"TLS","country":"EastTimor"}
+{ "index": {  }}
+{"code":"ECU","country":"Ecuador"}
+{ "index": {  }}
+{"code":"EGY","country":"Egypt"}
+{ "index": {  }}
+{"code":"SLV","country":"ElSalvador"}
+{ "index": {  }}
+{"code":"GNQ","country":"Equatorial Guinea"}
+{ "index": {  }}
+{"code":"ERI","country":"Eritrea"}
+{ "index": {  }}
+{"code":"EST","country":"Estonia"}
+{ "index": {  }}
+{"code":"ETH","country":"Ethiopia"}
+{ "index": {  }}
+{"code":"FRO","country":"FaroeIslands"}
+{ "index": {  }}
+{"code":"FJI","country":"Fiji"}
+{ "index": {  }}
+{"code":"FIN","country":"Finland"}
+{ "index": {  }}
+{"code":"FRA","country":"France"}
+{ "index": {  }}
+{"code":"GUF","country":"French Guiana"}
+{ "index": {  }}
+{"code":"PYF","country":"French Polynesia"}
+{ "index": {  }}
+'
